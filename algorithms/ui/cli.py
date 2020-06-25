37fcd7bf94847aa9a7c7e7da5a6c277def442d75
Thais Marinho <thasmarinho@gmail.com>
2020-06-25 18:29:32 -0300

    Extract util functions from quick sort command

--- algorithms/ui/cli.py
+++ algorithms/ui/cli.py
@@ -1,35 +1,53 @@
 #!/usr/bin/python
 import click
+import os
 from algorithms.ordenation import QuickSort
+
+
+def arg_to_list(args):
+    option = args
+    try:
+        option = str(option)
+    except ValueError:
+        pass
+
+    option = option[1:-1]  # trim '[' and ']'
+
+    options = option.split(',')
+
+    for i, value in enumerate(options):
+        # catch integers
+        try:
+            int(value)
+        except ValueError:
+            options[i] = value
+        else:
+            options[i] = int(value)
+    return options
+
+
+def show(result):
+    print("Sorted list = ", end='')
+    print(result)
 
 
 class TerminalView():
 
+    @click.group()
+    def process():
+        pass
+
     @click.command()
-    @click.option("--input", "-i", "option", required=True,
+    @click.option("--input", "-i", "input", required=True,
                   help="List to be sorted (e.g. [1,2,3])",
                   )
-    def process(option):
-        try:
-            option = str(option)
-        # option = str(option)  # this also works
-        except ValueError:
-            pass
-
-        option = option[1:-1]  # trim '[' and ']'
-
-        options = option.split(',')
-
-        for i, value in enumerate(options):
-            # catch integers
-            try:
-                int(value)
-            except ValueError:
-                options[i] = value
-            else:
-                options[i] = int(value)
-
+    @click.option('--asc/--asc-order', "-a", default=True, help='Sort list in ascending order')
+    def quick(input, asc):
+        """Sort a list using QUICK SORT algorithm.
+        """
+        list = arg_to_list(input)
         algo = QuickSort()
-        obtained = algo.sort(options)
-        print("Sorted list = ", end='')
-        print(obtained)
+        obtained = algo.sort(list)
+        show(obtained)
+
+    process.add_command(quick)
